dd77137d07ab0171b4b000004af3f5dd51788818
Daniel <danielbass123456@gmail.com>
2020-04-17 16:57:39 -0500

    clean up

--- fly.js
+++ fly.js
@@ -6,7 +6,7 @@ const dgram = require("dgram")
 
 const PORT = 8889
 const HOST = '192.168.10.1'
-const drone = dgram.createSocket('udp4')
+const drone = dgram.createSocket('udp4')    // talk to bot via udp packets
 drone.bind(PORT)
 
 drone.on('message', message => {
@@ -15,13 +15,12 @@ drone.on('message', message => {
 
 function handleError(err) {
   if (err) {
-    console.log('ERROR')
     console.log(err)
   }
 }
 
-drone.send('command', 0, 7, PORT, HOST, handleError)
-drone.send('battery?', 0, 8, PORT, HOST, handleError)
+drone.send('command', 0, 7, PORT, HOST, handleError)    // send sdk activate command
+drone.send('battery?', 0, 8, PORT, HOST, handleError)   // check battery level
 
 // drone.send('takeoff', 0, 7, PORT, HOST, handleError)
 // setTimeout(1000)
@@ -29,30 +28,67 @@ drone.send('battery?', 0, 8, PORT, HOST, handleError)
 
 rl.on('line', handleInput)
 function handleInput(line) {
-    console.log(line.split(" "))
     line = line.split(" ")
-    if (line.length > 1)
+
+    /*
+    Declare any used variables here.
+    Set at beginning to erase any past values.
+    */
+    var command = undefined;
+    var commandName = undefined;
+    var commandLen = 0;
+    var args = [];
+    var numArgs = 0;
+    var argsLen = 0;
+    var packetLen = 0;
+    var sendCmd = undefined;
+
+    /*
+    Some classy nested if loops to parse out
+    command type. Works down the three types
+    and if not found throws an error.
+    */
+    command = cmd.control[line[0]]
+    if (!command) {
+        command = cmd.set[line[0]]
+        if (!command) {
+            command = cmd.read[line[0]]
+            if (!command)
+            {
+                throw "Invalid command name error."
+            }
+        }
+    }
+
+    line.shift()                                // extract the args
+    args = line                                 // set args
+    if (args.length != command.args)            // quick check for correct arg amount
     {
-
+        throw "Invalid number of arguments"
     }
 
-    switch (line) {
-        case "exit":
-            return;
-        case "takeoff":
-            console.log("Detected takeoff command.");
-            drone.send('takeoff', 0, 7, PORT, HOST, handleError);
-            break;
-        case "land":
-            console.log("Detected land command.");
-            drone.send('land', 0, 4, PORT, HOST, handleError);
-            break;
-        case "battery":
-            let batt = drone.send('battery?', 0, 8, PORT, HOST, handleError);
-            console.log(batt);
-        case "flip":
-            drone.send(`flip ${line[1]}`, 0, 6, PORT, HOST, handleError);
-        default:
-            break;
+    numArgs = command.args                      // set argument length
+    commandName = command.name                  // some commands have different names so just to be sure
+
+    for (let i = 0; i < numArgs; i++)
+    {
+        argsLen += args[i].length
     }
+
+    commandLen = commandName.length             // num of chrs in command 
+    packetLen = commandLen + argsLen + numArgs  // len cmd + len of each arg + # of spaces needed
+
+    /*
+    Most cases would allow for 'line' to be passed back by itself,
+    but in cases when cmd name differs, it's best to do this
+    */
+    sendCmd = `${commandName}${args.join(" ")}` // cmd name + args
+
+    // console.log(sendCmd, 0, packetLen, PORT, HOST)  // useful for debugging
+
+    /*
+    At this point should be safe to send packet
+    If not, then guess we'll find out
+    */
+    drone.send(sendCmd, 0, packetLen, PORT, HOST, handleError);
 }
